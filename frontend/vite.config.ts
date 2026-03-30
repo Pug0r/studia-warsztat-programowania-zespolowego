c1d771edeedef48e7cb4321ac7fd07d6cf692431
Aleksander Pugowski <aleksanderpugowski@gmail.com>
2026-03-30 19:02:29 +0200

    Run the now required linting rules

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, 
+    host: true,
     port: 3000,
     watch: {
-      usePolling: true
+      usePolling: true,
     },
     proxy: {
-      '/api': {
-        target: 'http://backend:5000',
+      "/api": {
+        target: "http://backend:5000",
         changeOrigin: true,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
