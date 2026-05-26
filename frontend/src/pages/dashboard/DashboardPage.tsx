803644452f76fa8cead352c7b623efe1f77f0756
ratman <szczuras7@proton.me>
2026-05-26 21:09:03 +0200

    fix import

--- frontend/src/pages/dashboard/DashboardPage.tsx
+++ frontend/src/pages/dashboard/DashboardPage.tsx
@@ -10,7 +10,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { WalkMonitoringPanel } from "./WalkMonitoringPanel";
+import { WalkMonitoringPanel } from "../../modules/volunteers/components/WalkMonitoringPanel";
 
 export function DashboardPage() {
   const [date, setDate] = useState<Date | undefined>(new Date());
